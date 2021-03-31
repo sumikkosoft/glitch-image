d313172ed63747677366fc9734ccf60ceac844ee
ivgtr <gear.1112t.o@gmail.com>
2021-03-31 11:22:50 +0900

    :bug: fix: deploy設定の修正

--- vite.config.ts
+++ vite.config.ts
@@ -2,10 +2,7 @@ import reactPlugin from '@vitejs/plugin-react-refresh'
 import type { UserConfig } from 'vite'
 
 const config: UserConfig = {
-  plugins: [reactPlugin()],
-  build: {
-    outDir: 'docs'
-  }
+  plugins: [reactPlugin()]
 }
 
 export default config
