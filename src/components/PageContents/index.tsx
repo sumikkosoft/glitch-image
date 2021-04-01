8e0130c6b8e20a68ced9b733565e8aad4bf0fc08
ivgtr <gear.1112t.o@gmail.com>
2021-04-01 12:21:36 +0900

    :sparkles: add: Pattern Bの土台を作る

--- src/components/PageContents/index.tsx
+++ src/components/PageContents/index.tsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import { VersionOne } from '../VersionOne'
+import { VersionTwo } from '../VersionTwo'
 
 export const PageContents = () => {
   return (
     <div className="grid grid-cols-fill gap-4 mt-16 mx-8">
       <VersionOne />
+      <VersionTwo />
     </div>
   )
 }
